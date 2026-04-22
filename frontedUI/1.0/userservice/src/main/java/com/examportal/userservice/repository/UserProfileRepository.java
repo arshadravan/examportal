3696d1bb48a075e7a2462984e4884fb1b2e02fec
Arshad Hassan <arshhassan40@gmail.com>
2026-04-22 10:46:06 +0530

    add frontend 1.0

--- frontedUI/1.0/userservice/src/main/java/com/examportal/userservice/repository/UserProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.examportal.userservice.repository;
-
-import com.examportal.userservice.entity.UserProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
-}
